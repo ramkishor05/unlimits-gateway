22ee068c2e8a8c71bb12e1f406dc8f0eb9d7e778
ram kishor <ramkishor0509@gmail.com>
2019-12-26 20:46:01 +0530

    ok

--- src/main/java/com/brijframework/inventory/model/EOCategory.java
+++ src/main/java/com/brijframework/inventory/model/EOCategory.java
@@ -2,14 +2,17 @@ package com.brijframework.inventory.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="CATEGORY")
 public  class EOCategory implements Serializable{
 
 	/**
@@ -19,21 +22,84 @@ public  class EOCategory implements Serializable{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	public long id;
+	private long id;
 	
-	public String categoryID;
+	@Column(name="CATEGORY_ID")
+	private String categoryId;
 	
-	public String name;
-	public String description;
-	public String typeID;
+	@Column(name="NAME")
+	private String name;
+	
+	@Column(name="DESC")
+	private String description;
+	
+	@Column(name="TYPE")
+	private String type;
 
 	@ManyToOne
 	@JoinColumn(name="InvAppID",nullable=false)
-    public EOInvApp eoInvApp;
+	private EOInvApp eoInvApp;
 	
 	@ManyToOne
 	@JoinColumn(name = "CategoryGroupID",nullable=false)
-	public EOCategoryGroup eoCategoryGroup;
+	private EOCategoryGroup categoryGroup;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoryID() {
+		return categoryId;
+	}
+
+	public void setCategoryID(String categoryID) {
+		this.categoryId = categoryID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type= type;
+	}
+
+	public EOInvApp getEoInvApp() {
+		return eoInvApp;
+	}
+
+	public void setEoInvApp(EOInvApp eoInvApp) {
+		this.eoInvApp = eoInvApp;
+	}
+
+	public EOCategoryGroup getCategoryGroup() {
+		return categoryGroup;
+	}
+
+	public void setCategoryGroup(EOCategoryGroup categoryGroup) {
+		this.categoryGroup = categoryGroup;
+	}
+
 	
 	
 }
