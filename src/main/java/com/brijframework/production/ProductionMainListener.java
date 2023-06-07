35c1f0fa5ed4d5617d967725cd8eeef6e90d9cc8
krishnaenterprises0423 <krishnaenterprises0423@gmail.com>
2023-06-07 20:34:44 +0530

    in

--- src/main/java/com/brijframework/production/ProductionMainListener.java
+++ src/main/java/com/brijframework/production/ProductionMainListener.java
@@ -1,11 +1,15 @@
 package com.brijframework.production;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import com.brijframework.production.entities.cust.EOCustProductionApp;
 import com.brijframework.production.repository.GlobalCountFreqRepository;
+import com.brijframework.production.repository.cust.CustProductionAppRepository;
 
 @Component
 public class ProductionMainListener implements ApplicationListener<ContextRefreshedEvent> {
@@ -13,9 +17,18 @@ public class ProductionMainListener implements ApplicationListener<ContextRefres
 	@Autowired
 	GlobalCountFreqRepository countFreqRepository;
 	
+	@Autowired
+	CustProductionAppRepository custProductionAppRepository;
+	
     @Override
     public void onApplicationEvent(final ContextRefreshedEvent event) {
     	
-    	
+    	Optional<EOCustProductionApp> findCustProductionApp = custProductionAppRepository.findById(1L);
+    	if(!findCustProductionApp.isPresent()) {
+    		EOCustProductionApp custProductionApp =new EOCustProductionApp();
+    		custProductionApp.setCustId(1l);
+    		custProductionApp.setAppid(1l);
+    		custProductionAppRepository.saveAndFlush(custProductionApp);
+    	}
     }
 }
